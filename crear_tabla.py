3df7a94c5f6ba430bd2c65df0a3aff761788a3d5
Global Name <AL07102980@tecmilenio.mx>
2026-04-30 15:53:50 -0600

    Archivos Subidos

--- crear_tabla.py
+++ crear_tabla.py
@@ -1,21 +1,22 @@
 import boto3
-from datetime import datetime
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
 def crear_tabla():
-    tabla = dynamodb.create_table(
-        TableName='Base_finanzas',
-        KeySchema=[
-            {'AttributeName': 'id', 'KeyType': 'HASH'}
-        ],
-        AttributeDefinitions=[
-            {'AttributeName': 'id', 'AttributeType': 'S'}
-        ],
-        BillingMode='PAY_PER_REQUEST'
-    )
-    tabla.wait_until_exists()
-    print("✅ Tabla creada exitosamente")
-    return tabla
+    try:
+        tabla = dynamodb.create_table(
+            TableName='Base_finanzas',
+            KeySchema=[
+                {'AttributeName': 'id', 'KeyType': 'HASH'}
+            ],
+            AttributeDefinitions=[
+                {'AttributeName': 'id', 'AttributeType': 'S'}
+            ],
+            BillingMode='PAY_PER_REQUEST'
+        )
+        tabla.wait_until_exists()
+        print("Tabla creada exitosamente")
+    except dynamodb.meta.client.exceptions.ResourceInUseException:
+        print("La tabla ya existe, continuando...")
 
 crear_tabla()
