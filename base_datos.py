a3d2c58bcbe0890d117fc1c471017adc0a95ec77
Global Name <AL07102980@tecmilenio.mx>
2026-04-30 15:56:52 -0600

    Archivos Subidos

--- base_datos.py
+++ base_datos.py
@@ -1,6 +1,7 @@
 import boto3
 import random
 from datetime import datetime, timezone
+from decimal import Decimal
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 tabla = dynamodb.Table('Base_finanzas')
@@ -18,14 +19,14 @@ productos = ["Laptop", "Monitor", "Teclado", "Mouse", "Impresora",
 with tabla.batch_writer() as batch:
     for i in range(1, 51):
         batch.put_item(Item={
-            'id': f'CLI-{i:04d}',                          
-            'nombre': random.choice(nombres),               
-            'ciudad': random.choice(ciudades),              
-            'producto': random.choice(productos),           
-            'monto_venta': round(random.uniform(500, 50000), 2),  
+            'id': f'CLI-{i:04d}',
+            'nombre': random.choice(nombres),
+            'ciudad': random.choice(ciudades),
+            'producto': random.choice(productos),
+            'monto_venta': Decimal(str(round(random.uniform(500, 50000), 2))),
             'fecha_registro': datetime.now(timezone.utc).isoformat(),
             'modificado_por': 'sistema',
             'historial_cambios': []
         })
 
-print("✅ 50 registros insertados correctamente")
+print("50 registros insertados correctamente")
